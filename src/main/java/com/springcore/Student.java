697b884047e61adadb3122587081b1626094f679
Aniket rathore <Aniketrathore911@gmail.com>
2025-01-16 12:03:47 +0530

    Dependency Injection using Setter Method : Primitvite Data Types,Collection Types ,Reference Types

---
+++ src/main/java/com/springcore/Student.java
@@ -0,0 +1,41 @@
+package com.springcore;
+
+public class Student {
+	
+ private int studentId;
+ private String studentAddress;
+ private String studentName;
+public int getStudentId() {
+	return studentId;
+}
+@Override
+public String toString() {
+	return "Student [studentId=" + studentId + ", studentAddress=" + studentAddress + ", studentName=" + studentName
+			+ "]";
+}
+public Student() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+public Student(int studentId, String studentAddress, String studentName) {
+	super();
+	this.studentId = studentId;
+	this.studentAddress = studentAddress;
+	this.studentName = studentName;
+}
+public void setStudentId(int studentId) {
+	this.studentId = studentId;
+}
+public String getStudentAddress() {
+	return studentAddress;
+}
+public void setStudentAddress(String studentAddress) {
+	this.studentAddress = studentAddress;
+}
+public String getStudentName() {
+	return studentName;
+}
+public void setStudentName(String studentName) {
+	this.studentName = studentName;
+}
+}
