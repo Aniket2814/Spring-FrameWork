ec5ef8507df8e66c79a8706df329ab734e011fd1
Aniket rathore <Aniketrathore911@gmail.com>
2025-01-21 23:59:14 +0530

    Bean Scope:Singelton,Prototype

--- src/main/java/com/springcore/stereotype/Test.java
+++ src/main/java/com/springcore/stereotype/Test.java
@@ -11,6 +11,17 @@ public class Test {
 	System.out.println(s);
 	System.out.println(s.getAddress().getClass().getName());
 	System.out.println(s.getSalary());
+	
+	System.out.println(s.hashCode());
+Student s1=	con.getBean("student" ,Student.class);
+	
+	System.out.println(s1.hashCode());
+	
+	Teacher t1=con.getBean("t1",Teacher.class);
+
+	Teacher t2=con.getBean("t1",Teacher.class);
+	System.out.println(t1.hashCode());
+	System.out.println(t2.hashCode());
 	}
 
 }
