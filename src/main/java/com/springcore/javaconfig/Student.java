713a17d5be7df0ba4dac3f73ea7433aaf1faaebe
Aniket rathore <Aniketrathore911@gmail.com>
2025-01-23 17:38:29 +0530

    Removing Complete XML for Spring Confifguration:@Component,@ComponentScan,@Configuration

---
+++ src/main/java/com/springcore/javaconfig/Student.java
@@ -0,0 +1,13 @@
+package com.springcore.javaconfig;
+
+import org.springframework.stereotype.Component;
+
+@Component("st")
+public class Student {
+
+	
+	public void study()
+	{
+		System.out.println("Aniket is Studying");
+	}
+}
